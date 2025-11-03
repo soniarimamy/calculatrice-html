5b3406ad9f0f8e25bdadf9a420c987ff74ffc615
NantenainaRochel2 <soniarimamys@gmail.com>
2025-11-03 03:35:53 +0100

    feat: ajout affichage résultat et bouton reset

--- script.js
+++ script.js
@@ -23,3 +23,44 @@ function setOperation(op) {
     operation = op;
     shouldResetScreen = true;
 }
+function calculate() {
+    let result;
+    const prev = parseFloat(previousInput);
+    const current = parseFloat(currentInput);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            result = addition(prev, current);
+            break;
+        case '-':
+            result = soustraction(prev, current);
+            break;
+        case '*':
+            result = multiplication(prev, current);
+            break;
+        case '/':
+            result = division(prev, current);
+            break;
+        default:
+            return;
+    }
+    currentInput = result.toString();
+    operation = null;
+    previousInput = '';
+    updateDisplay();
+}
+function reset() {
+    currentInput = '0';
+    previousInput = '';
+    operation = null;
+    updateDisplay();
+}
+function appendNumber(number) {
+    if (currentInput === '0' || shouldResetScreen) {
+        currentInput = number;
+        shouldResetScreen = false;
+    } else {
+        currentInput += number;
+    }
+    updateDisplay();
+}
